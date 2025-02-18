0ba06c1ff1e9a39fcd1ead29859bc9518cd16883
Sonu08 <ssonu3628@gmail.com>
2025-02-18 11:41:36 +0100

    Initial gh-pages commit

---
+++ .next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site/',
+};
+
+module.exports = nextConfig;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
